d25ee94b14ca55fe09a3594e657c0e3546bc3c85
NoeFabris <noe@elysresidence.com>
2021-05-06 01:30:54 +0200

    Pt. 5 Added search and msg send

--- app.js
+++ app.js
@@ -4,7 +4,25 @@ const app = new Vue ({
         el: '#boolzapp', 
         data: {
             usersList: globalUsersList,
-            selectedUser: {}
+            selectedUser: {},
+            searchedUsers: {},
+            searchInput: '',
+            newMsg: ''
+        },
+        computed: {
+            searchedChat() {
+                if (!this.searchInput) {
+                    console.log('no ricerche')
+                    this.searchedUsers = this.usersList
+                } else {
+                    console.log('ricerca')
+
+                    let search = this.usersList.filter((searchName) => searchName.name == this.searchInput )
+                    console.log(search)
+                    this.searchedUsers = search
+
+                }
+            }
         },
         methods: {
             selectedContact(contact){
@@ -12,6 +30,25 @@ const app = new Vue ({
             },
             msgTime(time) {
                 return moment(time, 'DD/MM/YYY HH:mm:ss').format('HH:mm')
+            },
+            sendNewMsg() {
+                let msgFormat = {
+                    date: moment().format('DD/MM/YYY HH:mm:ss'),
+                    text: this.newMsg,
+                    status : 'sent'
+                }
+                this.selectedUser.messages.push(msgFormat);
+
+                setTimeout(() => {
+                    let msgResponseFormat = {
+                        date: moment().format('DD/MM/YYY HH:mm:ss'),
+                        text: 'Ok',
+                        status : 'received'
+                    }
+                    this.selectedUser.messages.push(msgResponseFormat)
+                }, 1000) ()
+                // response() ({
+                // } 1000)
             }
 
         }, 
